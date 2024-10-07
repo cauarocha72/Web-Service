4252910bc9484a903df6744936d0bb59531414fe
cauarocha72 <150359289+cauarocha72@users.noreply.github.com>
2024-10-07 11:16:04 -0300

    Add files via upload

--- Web Services (Cauã Rocha)/registro.js
+++ Web Services (Cauã Rocha)/registro.js
@@ -1,4 +1,48 @@
+nome = document.getElementById("nome").value
+ender = document.getElementById("ender").value
+email = document.getElementById("email").value
+tel = document.getElementById("tel").value
+
+connome = document.getElementById("connome").value
+conender = document.getElementById("conender").value
+conemail = document.getElementById("conemail").value
+contel = document.getElementById("contel").value
+
 function Entrar(){
-alert("foi registrado")
-window.location.href = "principal.html"
+alert("Por favaor, faz a confirmação para entrar")
+
+}
+function Confirma(){
+nome = document.getElementById("nome").value
+ender = document.getElementById("ender").value
+email = document.getElementById("email").value
+tel = document.getElementById("tel").value
+
+connome = document.getElementById("connome").value
+conender = document.getElementById("conender").value
+conemail = document.getElementById("conemail").value
+contel = document.getElementById("contel").value
+   
+if (nome == connome){
+    if (ender == conender){
+        if (email == conemail){
+            if (tel == contel){
+                alert("Login concluido")
+                window.location.href = "principal.html"
+            }
+            if (tel != contel){
+                alert("Ocorreu um erro na confirmação")
+            }
+        }
+        if(email != conemail){
+            alert("Ocorreu um erro na confirmação")
+        }
+    } 
+    if(ender != conender){
+        alert("Ocorreu um erro na confirmação")
+    }  
+}
+if(nome != connome){
+    alert("Ocorreu um erro na confirmação")
+}
 }
